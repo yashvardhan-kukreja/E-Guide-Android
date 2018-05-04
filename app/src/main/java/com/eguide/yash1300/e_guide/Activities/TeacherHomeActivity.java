e8b7c78515c8cc95b880bc89a0040e1c1c7904ef
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-05-05 03:15:20 +0530

    Rearranged and Refactored two activites

--- app/src/main/java/com/eguide/yash1300/e_guide/Activities/TeacherHomeActivity.java
+++ app/src/main/java/com/eguide/yash1300/e_guide/Activities/TeacherHomeActivity.java
@@ -1,7 +1,9 @@
-package com.eguide.yash1300.e_guide;
+package com.eguide.yash1300.e_guide.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.eguide.yash1300.e_guide.R;
 
 public class TeacherHomeActivity extends AppCompatActivity {
 
